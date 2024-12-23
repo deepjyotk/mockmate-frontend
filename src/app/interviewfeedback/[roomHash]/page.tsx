876236e222229edfd75910f33cfc539247af982f
Deepjyot Singh Kapoor <kapoordeepjyotsingh29@gmail.com>
2024-12-23 07:02:56 -0500

    check deployment

--- src/app/interviewfeedback/[roomHash]/page.tsx
+++ src/app/interviewfeedback/[roomHash]/page.tsx
@@ -10,7 +10,7 @@ const InterviewFeedbackPage = async ({ params }: { params: Params; }) => {
 
     // const { roomHash } = useParams(); // Access the dynamic route parameter
 
-console.log("room hash: " + params.roomHash);
+console.log("room hash:" + params.roomHash);
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100 text-gray-800">
         <InterviewFeedbackClient roomID={params.roomHash} />
